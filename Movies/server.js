eb1e68d9c6aabde574d5d763de6ca51e7d0c8168
williambaumbach1212 <will@dailygrinder.com>
2017-08-08 18:08:20 -0400

    Initial Commit

---
+++ Movies/server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+    //All web scraping magic will go here.
+    //url we will scrape from.
+    url = 'http://www.imdb.com/chart/top';
+    request(url, function(error, response, html){
+        //First check for errors.
+        if(!error){
+            var $ = cheerio.load(html);
+            var title, release, rating, ranking, n;
+            var json = {"Movie":[]}
+            n=1
+
+            $('.titleColumn').each(function(){
+                var data = $(this);
+                title = data.children('a').text();
+                rating = data.next().children().text();
+                json['Movie'].push({"ranking":n, "title":title, "rating":rating});
+                n++;
+            })
+
+        }
+
+        fs.writeFile('Top250Movies.json', JSON.stringify(json, null, 4), function(err){
+             console.log('File successfully written! - Check your project directory for the output.json file');
+        })
+
+        res.send('Check your console!')
+
+    })
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
